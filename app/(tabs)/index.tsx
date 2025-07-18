9baebff5c9842b28d4b46696d94f735ccbcc3789
Myko Miparanum <m.miparanum@gmail.com>
2025-07-18 18:15:30 +0800

    added assets & implement tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const TabIndex = () => {
+  return (
+    <View>
+      <Text>ops</Text>
+    </View>
+  );
+};
+
+export default TabIndex;
+
+const styles = StyleSheet.create({});
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native';
 import './global.css';
+
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-primary">Edit app/index.tsx to edit this screen.222</Text>
+      <Text className="text-5xl text-primary">Edit aasdaspp/index.tsx to edit this screen.222</Text>
     </View>
   );
 }
