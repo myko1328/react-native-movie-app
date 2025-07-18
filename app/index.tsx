f801a6722465f3151047c19db9cf8a0a3c45e83c
Myko Miparanum <m.miparanum@gmail.com>
2025-07-18 12:28:34 +0800

    react native boiletplate with nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+import './global.css';
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary">Edit app/index.tsx to edit this screen.222</Text>
+    </View>
+  );
+}
